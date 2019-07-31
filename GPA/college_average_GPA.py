9985efa77d53a922add92652613067ad509e6494
xuejun <xuejun0131@gmail.com>
2019-07-31 15:40:15 +0800

    增加：学院gpa结果分年级，代课最多的十位老师所属的学院

--- GPA/college_average_GPA.py
+++ GPA/college_average_GPA.py
@@ -9,15 +9,22 @@ print('开始生成学院平均GPA...')
 # 计算各院的平均GPA
 
 colleges = grades['stu_college'].value_counts().index.to_list()
+years = grades['year'].value_counts().index.to_list()
 
 gpa_list = []
+year = []
+college = []
 
-for i in colleges:
-    credit = grades[grades['stu_college'] == i]['credit'].sum()
-    gpa_list.append(grades[grades['stu_college'] == i]['weighed_gpa'].sum() / credit)
+for j in years:
+    for i in colleges:
+        college.append(i)
+        year.append(j)
+        credit = grades[(grades['stu_college'] == i) & (grades['year'] == j)]['credit'].sum()
+        gpa_list.append(grades[(grades['stu_college'] == i) & (grades['year'] == j)]['weighed_gpa'].sum() / credit)
 
-college_GPA = pd.DataFrame({'college': colleges, 'gpa': gpa_list})
+college_GPA = pd.DataFrame({'college': college, 'gpa': gpa_list, 'year': year})
 
-college_GPA.to_csv(path+'/res/college_average_gpa.csv')
+college_GPA.groupby(['college', 'year'])['gpa'].mean().to_excel(path+'res/gpa/college_average_gpa.xlsx')
+
 
 print('学院平均GPA生成完毕')
--- misc.py
+++ misc.py
@@ -6,4 +6,21 @@ grades = pd.read_csv(path + 'final_grade_table.csv')
 
 
 # 统计上课最多的老师
-grades.drop_duplicates('name')['instructor'].value_counts()[:10].to_excel(path+'/res/teacher.xlsx')
+teachers = grades.drop_duplicates('name')['instructor'].value_counts()[:10]
+
+teacher_name = teachers.index.to_list()
+
+teacher_num = teachers.to_list()
+
+teachers = pd.DataFrame([teacher_name, teacher_num]).T
+
+teachers.columns = [['teacher_name', 'num']]
+
+
+def match_college(row):
+    return grades[grades['instructor'] == str(row[0])]['course_college'].dropna().iloc[0]
+
+
+teachers['college'] = teachers.apply(match_college, axis=1)
+
+teachers.to_excel(path+'/res/misc/teacher.xlsx')
